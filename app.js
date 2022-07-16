b3ca1e0479f42d0be99f31634cb05804c19db59e
Anurag Shukla <tachyonanurag@gmail.com>
2022-07-16 15:32:22 +0530

    replaced crappy code with beautiful code  xD

--- app.js
+++ app.js
@@ -1,45 +1,38 @@
 const express = require("express")
-const path = require('path')
 const crd = require('./cred')
 const nodemailer = require('nodemailer')
-const router = express.Router();
 const port = process.env.port || 8000;
-
 const app = express()
-
-app.use('/', router)
-
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/template/index.html'))
-    
+const  mail = nodemailer.createTransport({
+	service:'gmail',
+	auth: {
+		user: crd.user,
+		pass: crd.pass,
+	}
 })
-
-router.post('/send', (req, res) => {
-    let smail = req.query.smail;
-    let rmail = req.query.rmail;
-    let subject = req.query.subject;
-    let message= req.query.message;
-
-    let mail = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        auth: {
-            user: crd.user,
-            pass: crd.pass,
-        }
-    })
-    mail.sendMail({
-        from: 'timeforchange9211@gmail.com',
-        to: [rmail],
-        subject: subject,
-        text: message
-    }, (err)=>{
-    if(err) throw err;
-    res.send(`mail has been sent`)
+app.use(express.json());
+app.use(express.urlencoded({
+	extended: true
+}))
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/template/index.html')
 })
+app.post('/send', (req, res) => {
+	console.log(req.body)
+	let rmail = req.body.rmail;
+	let subject = req.body.subject;
+	let message= req.body.message;
+	mail.sendMail({
+		from: crd.user,
+		to: [rmail],
+		subject: subject,
+		text: message
+	}, (err)=>{
+		if(err) throw err;
+		res.json({msg:`mail has been sent`})
+	})
 })
-
-
 app.listen(port , () => {
-    console.log(`listening on port ${port}`);
-    
+	console.log(`listening on port ${port}`);
+
 })
